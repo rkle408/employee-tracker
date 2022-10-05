a9c787b5c20b795d6c892378b66868412403c31d
Rosebud <rkle408@gmail.com>
2022-10-04 22:06:05 -0700

    view functions working, notes for possible way to receive data

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const connection = mysql.createConnection({
   database: 'employee_db'
   },
 
-  console.log(`Connected to the employee_db database.`)
+  console.log(`You are now connected to the employee database.\n`)
 );
 
 // Ask questions here:
@@ -49,23 +49,70 @@ function init() {
                 case 'View all departments':
                     viewDepartments();
                     break;
+                
+                case 'View all roles':
+                    viewRoles();
+                    break;
+
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+
+                // default: 
+                //     add(answers.action)
             }
     });
 };
 
 // Need to make functions to execute showing data and saving data
 function viewDepartments() {
-    var query = 'SELECT id, department.name AS Department FROM department ';
+    var query = `SELECT id, department.name AS Department 
+    FROM department`;
     connection.query(query, function(err, res) {
         console.table(res);
+        console.log('\n');
         init();
     });
 };
 
-// function viewRoles()
+function viewRoles() {
+    var query = `SELECT role.id, role.title, department.name AS department, role.salary 
+    FROM role 
+    JOIN department ON role.department_id = department.id`;
+    connection.query(query, function(err, res) {
+    console.table(res);
+    init();
+    });
+};
+
+function viewEmployees() {
+    var query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager ON manager.id = employee.manager_id
+    JOIN role ON employee.role_id = role.id
+    JOIN department ON role.department_id = department.id
+    ORDER BY employee.id`;
+    connection.query(query, function(err, res) {
+    console.table(res);
+    init();
+    });
+};
 
 
-// function viewEmployees()
+// // CAN Use function that can do multiple jobs to execute to minimize code
+// function add(type) {
+//     let questions;
+//     // Run the 3 adds
+//     switch (type) {
+//         case 'Add a department':
+//             questions = departmentQuestion
+//         case 'Add a role':
+//             questions = roleQuestions
+//         case 'Add an employee':
+//             questions = employeeQuestions
+//     }
+//     inquirer.prompt(questions)
+// }
 
 // Will likely move adding actions here and remove from questions.js
 
